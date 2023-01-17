e4c2a52383633097613403fe64dbd7cc0c6a70e3
JoshuaHamann <joshwashku@yahoo.com>
2023-01-17 11:08:17 -0600

    added a route in server.js to notes.html when selected

---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+//app.get('/', (req,res) => res.send('navigate to route'));
+
+app.get('/', (req,res) =>
+    res.sendFile(path.join(__dirname, 'public/assets/index.html'))
+);
+
+app.get('/notes', (req,res) =>
+    res.sendFile(path.join(__dirname, 'public/assets/notes.html'))
+);
+
+
+app.listen(PORT, () =>
+    console.log(`Example listening at http://localhost:${PORT}`)
+)
