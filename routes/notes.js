273ed4b666e3511a02169f111ea6f1e0e7f48834
JoshuaHamann <joshwashku@yahoo.com>
2023-01-19 10:26:24 -0600

    added a GET request to read and parse db JSON objects

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,13 @@
 const fb = require('express').Router();
+
+// Destruct functions from helper js file
+const {readFromFile, readAndAppend} = require('../helpers/fsUtils')
+
+// GET Route for retrieving all notes from db
+fb.get('/', (req,res) => {
+    console.info(`${req.method} request recieved for notes`);
+
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+module.exports = fb;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { appendFileSync } = require('fs');
 const path = require('path');
 const api = require('./routes/index.js');
 
