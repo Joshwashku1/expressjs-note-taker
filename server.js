511f3df1d8e427d2c0d5bfd7a709c82fcf9fe6ff
JoshuaHamann <joshwashku@yahoo.com>
2023-01-19 10:13:15 -0600

    Moduarlized notes route for api

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const { appendFileSync } = require('fs');
 const path = require('path');
+const api = require('./routes/index.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use('/api', api);
 
-//app.get('/', (req,res) => res.send('navigate to route'));
 
 app.get('/', (req,res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
