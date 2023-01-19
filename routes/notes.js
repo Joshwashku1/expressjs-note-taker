53f06052cc913baeef27a293e5b9045467cde49e
JoshuaHamann <joshwashku@yahoo.com>
2023-01-19 13:25:02 -0600

    added a post api route to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,32 @@
-const fb = require('express').Router();
+const notes = require('express').Router();
 
 // Destruct functions from helper js file
 const {readFromFile, readAndAppend} = require('../helpers/fsUtils')
 
 // GET Route for retrieving all notes from db
-fb.get('/', (req,res) => {
+notes.get('/', (req,res) => {
     console.info(`${req.method} request recieved for notes`);
 
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-module.exports = fb;
+notes.post('/', (req,res) => {
+    console.info(`${req.method} request recieved to add a note`);
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if(req.body) {
+        const newNote = {
+            title,
+            text,
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully!!`);
+    } else {
+        res.errored('Error in adding note');
+    }
+});
+
+module.exports = notes;
