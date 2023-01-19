3230cb400016ac52dbcf388e684be9de72a7f782
JoshuaHamann <joshwashku@yahoo.com>
2023-01-19 09:26:20 -0600

    changed files so path directories worked when starting up the server

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.use(express.static('public'));
 //app.get('/', (req,res) => res.send('navigate to route'));
 
 app.get('/', (req,res) =>
-    res.sendFile(path.join(__dirname, 'Develop/public/assets/index.html'))
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.get('/notes', (req,res) =>
-    res.sendFile(path.join(__dirname, 'Develop/public/assets/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
 
